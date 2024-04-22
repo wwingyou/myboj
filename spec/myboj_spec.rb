f9e7700435caff795ea419419cbb506d3095c3cd
wingyou <winwing.you@gmail.com>
2024-04-22 22:53:23 +0900

    gem 기본 설정

---
+++ spec/myboj_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Myboj do
+  it "has a version number" do
+    expect(Myboj::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
