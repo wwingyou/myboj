72d779e3641de88be34a8451b39e758a1b54bac0
wingyou <winwing.you@gmail.com>
2024-06-25 02:56:06 +0900

    feat: version command implemented

--- lib/myboj/cli.rb
+++ lib/myboj/cli.rb
@@ -4,12 +4,20 @@ require "thor"
 
 module Myboj
   class CLI < Thor
-    desc "새 문제 가져오기", "지정한 코드의 문제의 테스트케이스와 정보를 가져옵니다."
+    package_name "myboj"
+    map "-v" => :version
+
+    desc "version", "현재 버전을 출력합니다."
+    def version
+      puts "myboj version #{Myboj::VERSION}"
+    end
+
+    desc "pull [CODE]", "지정한 코드의 문제의 테스트케이스와 정보를 가져옵니다."
     def pull(code)
       puts "[1000번] A+B"
     end
 
-    desc "상태 출력", "현재 문제 정보, 언어, 로그인 상태 등을 출력합니다."
+    desc "status", "현재 문제 정보, 언어, 로그인 상태 등을 출력합니다."
     method_option :verbose, aliases: "-v"
     def status
       puts "status info"
